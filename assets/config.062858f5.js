6bcafa1d8984d3ca9189628de56166bf1cc02c41
Ethan Halko <ethan.halko@factorybucket.com>
2023-09-05 16:31:54 -0400

    Updates

---
+++ assets/config.062858f5.js
@@ -0,0 +1,1 @@
+import{O as p,P as o,Q as i,u as f}from"./entry.38edcafa.js";const s=p({nuxtIcon:{class:""}}),t={},a=o(s,t);function u(){const n=f();return n._appConfig||(n._appConfig=i(a)),n._appConfig}export{u};
